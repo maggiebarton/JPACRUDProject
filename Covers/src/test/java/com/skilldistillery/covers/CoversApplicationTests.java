63493456151f21ee3549ce1fbcef24e39c2e8167
Maggie Barton <maggiebarton2@gmail.com>
2023-09-15 17:35:22 -0600

    boot mvc with jpa connection

---
+++ Covers/src/test/java/com/skilldistillery/covers/CoversApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.covers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoversApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
